c3cda5ed03e1f7dff3b83a2f5a783f859af589c8
sinusen <sen_sinu@yahoo.com.au>
2022-11-08 21:07:13 +1100

    Testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Testing beanstalk deployment
         </p>
         <a
           className="App-link"
